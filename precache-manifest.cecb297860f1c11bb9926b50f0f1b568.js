f1b21867b7f49e365819a2b90434b11d611477d2
Fadhil <fadhil.akins@gmail.com>
2020-07-30 02:25:57 +0100

    Updates

---
+++ precache-manifest.cecb297860f1c11bb9926b50f0f1b568.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd33f2f3820d6ea9894f726416f7436",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "5311c1ecb6ebb20fe6cb",
+    "url": "/calculator/static/css/main.ba1c204a.chunk.css"
+  },
+  {
+    "revision": "f578502beb2fdc89eaf7",
+    "url": "/calculator/static/js/2.bca3a4f4.chunk.js"
+  },
+  {
+    "revision": "5311c1ecb6ebb20fe6cb",
+    "url": "/calculator/static/js/main.56026a49.chunk.js"
+  },
+  {
+    "revision": "4ed1cf36ed9e5a064592",
+    "url": "/calculator/static/js/runtime-main.b8dd65a4.js"
+  }
+]);
